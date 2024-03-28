4ae85ec7cb6635366762c2ba0990e86834140a0a
shahul cb <theshahulofcl@gmail.com>
2024-03-28 21:40:52 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 // const bodyParser = require("body-parser")
 
-app.use(cors({ credentials: true, origin: true }))
+app.use(cors({ credentials: true, origin: [process.env.ORIGIN] }))
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
