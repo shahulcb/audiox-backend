13b3538a53d6bbeb9c8271daab25cea1c7d1e6c0
shahul cb <theshahulofcl@gmail.com>
2024-03-29 16:09:44 +0530

    cors options changed

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
 const cookieParser = require("cookie-parser");
 // const bodyParser = require("body-parser")
 
-app.use(cors({ credentials: true, origin: true }))
+app.use(cors())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
