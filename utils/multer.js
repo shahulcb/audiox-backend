bfa9684ee8f2cf325acb8f956c70f3d805458674
shahul cb <theshahulofcl@gmail.com>
2024-03-30 00:45:50 +0530

    test

--- utils/multer.js
+++ utils/multer.js
@@ -2,6 +2,7 @@ const multer = require("multer")
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
+        console.log("test", req.file);
         cb(null, "./uploads");
     },
     filename: function (req, file, cb) {
